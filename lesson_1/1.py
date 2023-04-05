ce29ce160afe89ba7adfae67e00252ab64026b89
T1monation <4timonaton@gmail.com>
2023-04-05 09:50:03 +0000

    hm fix

--- lesson_1/1.py
+++ lesson_1/1.py
@@ -1,13 +1,25 @@
 # Задание 1.
-word_one = 'разработка'
-word_two = 'сокет'
-word_tree = 'декоратор'
-print(word_one, type(word_one))
-print(word_two, type(word_two))
-print(word_tree, type(word_tree))
-word_one = word_one.encode('utf-8')
-word_two = word_two.encode('utf-8')
-word_tree = word_tree.encode('utf-8')
-print(word_one, type(word_one))
-print(word_two, type(word_two))
-print(word_tree, type(word_tree))
+
+word_list = ['разработка', 'сокет', 'декоратор']
+for word in word_list:
+    print(word, type(word), 'после преобразования:',
+          word.encode('utf-8'), type(word.encode('utf-8')))
+
+# строки в формате utf-8
+unicode_list = [
+    '\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0',
+    '\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82',
+    '\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80',
+]
+for word in unicode_list:
+    print(word, type(word))
+
+# строки в формате utf-16
+unicode_list_16 = [
+    '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
+    '\u0441\u043e\u043a\u0435\u0442',
+    '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440',
+]
+
+for word in unicode_list_16:
+    print(word, type(word))
--- lesson_1/3.py
+++ lesson_1/3.py
@@ -1,10 +1,8 @@
 # Задание 3.
-word_one = b'attribute'
-# word_two = b'класс'    -> SyntaxError: bytes can only contain ASCII literal characters
-#                           а кирилицы в ASCII нет
-# word_tree = b'функция' -> SyntaxError: bytes can only contain ASCII literal characters
-word_fore = b'type'
-print(word_one, type(word_one), len(word_one))
-# print(word_two, type(word_two), len(word_two))
-# print(word_tree, type(word_tree), len(word_tree))
-print(word_fore, type(word_fore), len(word_fore))
+word_list = ['attribute', 'класс', 'функция', 'type']
+for word in word_list:
+    try:
+        print(bytes(word, 'ascii'))
+    except UnicodeEncodeError as error:
+        print(
+            f'Невозможно преобразовать "{word}" в байты, сообщение ошибки:\n {error}')
