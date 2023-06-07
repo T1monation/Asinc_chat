f0a2e48b5e78ceb7d0d8ecee94fa637cd08135d8
T1monation <4timonaton@gmail.com>
2023-06-07 21:51:53 +0300

    add PySide2 in project

---
+++ ChatWindow/chatwindow.py
@@ -0,0 +1,32 @@
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+from lesson_11.client import Client
+
+client = Client()
+
+class ChatWindow(QWidget):
+    def __init__(self):
+        super(ChatWindow, self).__init__()
+        self.load_ui()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = ChatWindow()
+    widget.show()
+    sys.exit(app.exec_())
