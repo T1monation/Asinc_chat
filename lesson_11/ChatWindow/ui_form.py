6f0a4f574913b492aa6774fc8e6e1a6d5ce1a764
T1monation <4timonaton@gmail.com>
2023-06-12 23:29:10 +0300

    del rubbish

--- lesson_11/ChatWindow/ui_form.py
+++ lesson_11/ChatWindow/ui_form.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'formiWVbMN.ui'
+## Form generated from reading UI file 'formwtLuTJ.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.4
 ##
