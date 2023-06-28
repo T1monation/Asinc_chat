3aad6ad14a5e154c3453ffae18027ac35081e440
T1monation <94930740+T1monation@users.noreply.github.com>
2023-06-28 10:29:11 +0300

    leson 15 done! (#15)

--- lesson_1/3.py
+++
@@ -1,8 +0,0 @@
-# Задание 3.
-word_list = ['attribute', 'класс', 'функция', 'type']
-for word in word_list:
-    try:
-        print(bytes(word, 'ascii'))
-    except UnicodeEncodeError as error:
-        print(
-            f'Невозможно преобразовать "{word}" в байты, сообщение ошибки:\n {error}')
